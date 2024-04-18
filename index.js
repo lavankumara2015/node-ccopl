c0495f17c2549c6421727bd2a6a2faee7ebac5b4
Lavan Adicherla <143772951+lavankumara2015@users.noreply.github.com>
2024-04-18 17:03:10 +0530

    update

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.get("/webhook", function (req, res) {
     req.query["hub.verify_token"] == "token"
   ) {
     // res.send(req.query["hub.challenge"]);
-    response.status(200).json({data});
+    res.status(200).json({data});
 
   } else {
     res.sendStatus(400);
