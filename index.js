813404bb062af981917cab9fbf644f32b289fc58
Lavan Adicherla <143772951+lavankumara2015@users.noreply.github.com>
2024-04-19 14:48:39 +0530

    update

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const path = require("path");
 const { MongoClient } = require("mongodb");
 const bodyParser = require("body-parser");
 const app = express();
@@ -10,6 +9,7 @@ const PORT = process.env.PORT || 3007;
 let client = new MongoClient(
   "mongodb+srv://sanjukanki56429:dmX96TLZGz7OYS9A@cluster0.eg2lxgb.mongodb.net/"
 );
+
 let db;
 
 let initializeDBAndServer = async (req, res) => {
@@ -46,5 +46,61 @@ app.post("/webhook", async function (request, response) {
     response.status(201).json({ msg: "Created Successfully" });
   } catch (error) {
     response.status(400).json({ msg: "Something Went Wrong" });
+  }
+});
+
+function getMessageObject(data, type = "text") {
+  if (type === "text") {
+    let messages = {
+      messaging_product: "whatsapp",
+      recipient_type: "individual",
+      to: "918096255759",
+      type: "text",
+      text: {
+        preview_url: false,
+        body: data.text,
+      },
+    };
+    return messages;
+  } else if (type === "reaction") {
+    return {
+      messaging_product: "whatsapp",
+      recipient_type: "individual",
+      to: "918096255759",
+      type: "reaction",
+      reaction: {
+        message_id:
+          "wamid.HBgMOTE4MDk2MjU1NzU5FQIAEhggNTNDQzU3MjNFRjAyMUI0NjM5MzNERDQ2NDMxRDFFMDQA",
+        emoji: data.emoji,
+      },
+    };
+  }
+}
+
+app.post("/message", async function (request, response) {
+  try {
+    const { type, data } = await request.body;
+    let formattedObject = getMessageObject(data, type);
+    const ourResponse = await fetch(
+      "https://graph.facebook.com/v19.0/232950459911097/messages",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          authorization:
+            "Bearer EABqxsZAVtAi8BO4TjmZBatrXr0IlGIZAOP7xVpt1zqCLZCOL5TPQ6n2Se3ZCeYTqJEa3LrssVHS8i9wpTwdSW81e7lRnBrLqjkETZCjRCvlp9ZBne5ZCpfOv86t4AWU1htKtEhvHfNM7eJIrSRThr5LIamYejQjmkVI1Bhi0UVctw0bWF1eTNknbTLAvOmTI11qgvorpeoGkOl9npZCfksEYZD",
+        },
+        body: JSON.stringify(formattedObject),
+      }
+    );
+    console.log(ourResponse);
+    if (ourResponse.ok) {
+      console.log("Yes all is well");
+      response.status(201).json({ msg: "Created Successfully" });
+    } else {
+      response.status(401).json({ msg: "Something Unexpected" });
+    }
+  } catch (error) {
+    response.status(400).json({ msg: `Something Went Wrong ${error.message}` });
   }
 });
