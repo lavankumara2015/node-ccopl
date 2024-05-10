d95b0ef0bcdbc0561b73833a230719cec9630d84
tejasshri <tejasshrishekhar08@gmail.com>
2024-05-10 13:00:18 +0530

    update

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const cors = require("cors");
 app.use(cors());
 app.use(express.json());
 require("dotenv").config();
-const PORT = process.env.PORT || 3007;
+const PORT = process.env.PORT || 3007 ;
 
 let db;
 
